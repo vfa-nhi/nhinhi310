b8e15e7955b1a8d54ba4842f201651e312179efc
Nguyen Nhi <vfa.nhintt@gmail.com>
2020-03-05 10:49:25 +0700

    Meo la meo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,8 +41,8 @@
       <div class="page-header-image" data-parallax="true" style="background-image: url('images/cc-bg-1.jpg');"></div>
       <div class="container">
         <div class="content-center">
-          <div class="cc-profile-image"><a href="#"><img src="images/anthony.jpg" alt="Image"/></a></div>
-          <div class="h2 title">Anthony Barnett</div>
+          <div class="cc-profile-image"><a href="#"><img src="images/meomeo.jpg" alt="Image"/></a></div>
+          <div class="h2 title">Meo la Meo</div>
           <p class="category text-white">Web Developer, Graphic Designer,  Photographer</p><a class="btn btn-primary smooth-scroll mr-2" href="#contact" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Hire Me</a><a class="btn btn-primary" href="#" data-aos="zoom-in" data-aos-anchor="data-aos-anchor">Download CV</a>
         </div>
       </div>
@@ -61,7 +61,7 @@
         <div class="col-lg-6 col-md-12">
           <div class="card-body">
             <div class="h4 mt-0 title">About</div>
-            <p>Hello! I am Anthony Barnett. Web Developer, Graphic Designer and Photographer.</p>
+            <p>Hello! I am Meo La Meo. Web Developer, Graphic Designer and Photographer.</p>
             <p>Creative CV is a HTML resume template for professionals. Built with Bootstrap 4, Now UI Kit and FontAwesome, this modern and responsive design template is perfect to showcase your portfolio, skills and experience. <a href="https://templateflip.com/templates/creative-cv/" target="_blank">Learn More</a></p>
           </div>
         </div>
